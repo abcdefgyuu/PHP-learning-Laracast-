fa999b388d02f5d7c273a1e9331598ba9dd3ff59
Aamatar <mtm.aamatar@gmail.com>
2025-09-11 16:48:36 +0630

    validation set up

--- views/notes.view.php
+++ views/notes.view.php
@@ -7,9 +7,10 @@
    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <ul>
       <?php foreach ($notes as $note) : ?>
-        <li class="list-disc ml-6 text-blue-600"><a href="note?id=<?= $note['id']?>" class="text-blue-600 hover:underline"><?= $note['body'] ?></a></li>
+        <li class="list-disc ml-6 text-blue-600"><a href="note?id=<?= $note['id']?>" class="text-blue-600 hover:underline"><?= htmlspecialchars($note['body']) ?></a></li>
       <?php endforeach; ?>
     </ul>
+    <p class="mt-6"><a href="/learning/notes/create" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md">Create Note</a></p>
    </div>
  </main>
 
