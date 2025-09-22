63cdda05abbcbacb01351890cf4bcc64cb39345d
Aamatar <mtm.aamatar@gmail.com>
2025-09-22 16:15:31 +0630

    intro to middleware

--- Core/Router.php
+++ Core/Router.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Core;
+use Core\Middleware\Middleware;
 
 class Router
 {
@@ -11,35 +12,43 @@ class Router
     $this->routes[] = [
       "uri" => $uri,
       "controller" => $controller,
-      "method" => $method
+      "method" => $method,
+      "middleware" => null
     ];
 
+    return $this;
   }
 
 
   public function get($uri, $controller)
   {
-    $this->add("GET", $uri, $controller);
+    return $this->add("GET", $uri, $controller);
   }
 
   public function post($uri, $controller)
   {
-    $this->add("POST", $uri, $controller);
+    return $this->add("POST", $uri, $controller);
   }
 
   public function delete($uri, $controller)
   {
-    $this->add("DELETE", $uri, $controller);
+    return $this->add("DELETE", $uri, $controller);
   }
 
   public function put($uri, $controller)
   {
-    $this->add("PUT", $uri, $controller);
+    return $this->add("PUT", $uri, $controller);
   }
 
   public function patch($uri, $controller)
   {
-    $this->add("PATCH", $uri, $controller);
+    return $this->add("PATCH", $uri, $controller);
+  }
+
+  public function only($key)
+  {
+    $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+    return $this;
   }
 
 
@@ -49,6 +58,9 @@ class Router
 
     foreach ($this->routes as $route) {
       if ($route["uri"] === $uri && $route['method'] === strtoupper($requestMethod)) {
+
+       Middleware::reslove($route['middleware']);
+        
         return require base_path($route['controller']);
       }
     }
